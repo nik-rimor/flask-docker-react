6253b639cfdbeda7419ba1b25004aba94d84fa8e
Nikos Kekes <nickkekes@gmail.com>
2018-12-23 01:00:59 +0200

    syntax corrections, testing code added

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,21 +1,28 @@
 # services/users/project/config.py
 
 
+import os
+
+
+
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
-class ProductionConfiguration(BaseConfig):
+class ProductionConfig(BaseConfig):
     """Production Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
